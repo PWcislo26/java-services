29eabdd33ae3c0aea05c382d8b75def47a195321
Piotr <92379337+PWcislo26@users.noreply.github.com>
2022-06-16 20:32:22 +0200

    Added student basic functionalities (get, post, delete)

---
+++ StudentsModule/src/main/java/com/example/studentsmodule/StudentsModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentsmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsModuleApplication.class, args);
+    }
+
+}
---
+++ StudentsModule/src/test/java/com/example/studentsmodule/StudentsModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentsmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
