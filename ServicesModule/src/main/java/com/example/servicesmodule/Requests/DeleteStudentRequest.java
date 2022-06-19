885fff755736e2f2095bdf01f821d7d31e2c3b8d
Piotr <92379337+PWcislo26@users.noreply.github.com>
2022-06-19 15:19:17 +0200

    Removed redundant imports

--- ServicesModule/src/main/java/com/example/servicesmodule/Requests/DeleteStudentRequest.java
+++ ServicesModule/src/main/java/com/example/servicesmodule/Requests/DeleteStudentRequest.java
@@ -2,9 +2,6 @@ package com.example.servicesmodule.Requests;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 
 @Getter
 @AllArgsConstructor
